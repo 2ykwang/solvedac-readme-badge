ed632a3c10cef44acaf086e782f498191a84b57f
Yeonggwang <immutable000@gmail.com>
2021-11-02 18:28:42 +0900

    add: solved ac data fetch

--- app/api/generate.py
+++ app/api/generate.py
@@ -1,16 +1,14 @@
 from flask import (
     jsonify
 )
+from ..solvedac import *
+import os
 
 
 def generate_badge_by_username():
-    data = jsonify({
-        "success": True,
-        "result": {
-            "username": f""
-        }
-    })
-    return data
+    fetcher = SolvedacFetcher(os.getenv("API_HOST"))
+    a = fetcher.get_user_info("csdp000")
+    return jsonify(a)
 
 
 def generate_card_by_username():
