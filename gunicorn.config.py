368dee1506bcd7292e73f851a43096b49f17a1fa
Yeonggwang <immutable000@gmail.com>
2021-11-02 16:45:59 +0900

    heroku 배포 설정

---
+++ gunicorn.config.py
@@ -0,0 +1,10 @@
+import multiprocessing
+import os
+
+# gunicorn - configuration
+wsgi_app = "app:create_app()"
+# bind = f'{os.getenv("HOST")}:{os.getenv("PORT")}'
+workers = multiprocessing.cpu_count() * 2 + 1
+
+# CMD gunicorn --check--config ./gunicorn.config.py
+# CMD gunicorn -c ./gunicorn.config.py
---
+++ procfile
@@ -0,0 +1,1 @@
+web: gunicorn -c ./gunicorn.config.py
