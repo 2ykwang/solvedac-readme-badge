e0fc9408e89d1cee2db9422914c6b324955bf624
2ykwang <immutable000@gmail.com>
2021-11-08 06:20:13 +0900

    refactor: User 인스턴스 생성코드 분리

--- app/solvedac/__init__.py
+++ app/solvedac/__init__.py
@@ -1,4 +1,3 @@
-from .helper import get_user_from_dict as get_user_from_dict
 
 from .solvedac_badge import make_badge as make_badge
 
@@ -8,5 +7,6 @@ from .tier import get_tier_text
 from .tier import get_tier_icon
 
 from .user import User as User
+from .user import get_user_from_dict as get_user_from_dict
 
 __version__ = "0.0.1"
--- app/solvedac/user.py
+++ app/solvedac/user.py
@@ -1,5 +1,40 @@
-
 class User:
     def __init__(self):
-        self.username = ""
-        self.tier = 0
+        self.username: str = ""
+        """solved.ac 유저 ID"""
+        self.bio: str = ""
+        """solved.ac 유저 Biography"""
+        self.tier: int = 0
+        """solved.ac 유저 티어 (0-31)"""
+        self.user_class: int = 0
+        """solved.ac 유저 클래스 (0-10)"""
+        self.user_class_decoration: str = ""
+        """solved.ac 유저 클래스 데코레이션 (none, silver, gold)"""
+        self.rating: int = 0
+        """solved.ac 유저 레이팅"""
+        self.exp: int = 0
+        """solved.ac 유저가 획득한 경험치"""
+        self.rank: int = 0
+        """solved.ac 유저 순위"""
+
+        self.json_raw_data: dict = {}
+        """solved.ac api 유저 raw data"""
+    def __str__(self):
+        return str(self.json_raw_data)
+
+
+def get_user_from_dict(json_data: dict) -> User:
+    result = User()
+
+    result.username = json_data["handle"]
+    result.bio = json_data["bio"]
+    result.tier = json_data["tier"]
+    result.user_class = json_data["class"]
+    result.user_class_decoration = json_data["classDecoration"]
+    result.rating = json_data["rating"]
+    result.exp = json_data["exp"]
+    result.rank = json_data["rank"]
+
+    result.json_raw_data = json_data
+
+    return result
