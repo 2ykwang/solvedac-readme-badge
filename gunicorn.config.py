cb3d5dc13e4fdfd045658b5aacf433468c2eefd4
Yeonggwang <immutable000@gmail.com>
2021-12-01 02:59:28 +0900

    chroe: edit gunicorn workers value

--- gunicorn.config.py
+++ gunicorn.config.py
@@ -4,7 +4,7 @@ import os
 # gunicorn - configuration
 wsgi_app = "run:app"
 # bind = f'{os.getenv("HOST")}:{os.getenv("PORT")}'
-workers = multiprocessing.cpu_count() * 2 + 1
+# workers = multiprocessing.cpu_count() * 2 + 1
 
 # CMD gunicorn --check--config ./gunicorn.config.py
 # CMD gunicorn -c ./gunicorn.config.py
