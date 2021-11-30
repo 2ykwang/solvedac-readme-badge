22c49c3dd459b42e53913ad9fc455439b547fc2f
Yeonggwang <immutable000@gmail.com>
2021-12-01 01:26:08 +0900

    fix: config error

--- config.py
+++ config.py
@@ -4,15 +4,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    API_HOST = os.getenv("API_HOST") or ""
-    TIMEOUT = int(os.getenv("TIMEOUT")) or 30
+    API_HOST = os.getenv("API_HOST") or "solved.ac"
+    TIMEOUT = int(os.getenv("TIMEOUT") or 30)
 
     # flask cache ( api call )
     CACHE_TYPE = "SimpleCache"
-    CACHE_DEFAULT_TIMEOUT = int(os.getenv("CACHE_USER_FETCH")) or 300  # api call cache
+    CACHE_DEFAULT_TIMEOUT = int(os.getenv("CACHE_USER_FETCH") or 300)  # api call cache
 
     # image cache
-    CACHE_CONTROL = int(os.getenv("CACHE_CONTROL")) or 600
+    CACHE_CONTROL = int(os.getenv("CACHE_CONTROL") or 600)
 
     @staticmethod
     def init_app(app):
