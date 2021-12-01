f8fd113a139a919f0c42be2d6ed39effcfabc1ac
Yeonggwang <immutable000@gmail.com>
2021-12-02 07:51:50 +0900

    chore: gunicorn workers 기본값 변경

--- gunicorn.config.py
+++ gunicorn.config.py
@@ -3,8 +3,8 @@ import os
 
 # gunicorn - configuration
 wsgi_app = "run:app"
+workers = multiprocessing.cpu_count() * 2 + 1
 # bind = f'{os.getenv("HOST")}:{os.getenv("PORT")}'
-# workers = multiprocessing.cpu_count() * 2 + 1
 
 # CMD gunicorn --check--config ./gunicorn.config.py
 # CMD gunicorn -c ./gunicorn.config.py
